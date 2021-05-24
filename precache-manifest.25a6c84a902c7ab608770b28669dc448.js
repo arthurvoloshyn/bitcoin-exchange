dcf934ff9f6d3580153a87c29d816f2f8bc1f2c9
arturvoloshyn <arthurvoloshyn@gmail.com>
2021-05-24 18:05:43 +0300

    Updates

---
+++ precache-manifest.25a6c84a902c7ab608770b28669dc448.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5c83d73a8936d6ef29e20e2dd99a2d",
+    "url": "/bitcoin-exchange/index.html"
+  },
+  {
+    "revision": "0bdc0ee3e40e6bb61af7",
+    "url": "/bitcoin-exchange/static/css/2.54c80412.chunk.css"
+  },
+  {
+    "revision": "a51b94200ba49cc56078",
+    "url": "/bitcoin-exchange/static/css/main.546ff56f.chunk.css"
+  },
+  {
+    "revision": "0bdc0ee3e40e6bb61af7",
+    "url": "/bitcoin-exchange/static/js/2.09978a83.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/bitcoin-exchange/static/js/2.09978a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89c08e32af4f1b4cc2e",
+    "url": "/bitcoin-exchange/static/js/3.fbf7663e.chunk.js"
+  },
+  {
+    "revision": "a51b94200ba49cc56078",
+    "url": "/bitcoin-exchange/static/js/main.5b25b54c.chunk.js"
+  },
+  {
+    "revision": "d1709912460de10ccd10",
+    "url": "/bitcoin-exchange/static/js/runtime-main.e73c2e15.js"
+  }
+]);
