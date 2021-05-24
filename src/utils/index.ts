e0c47b6cc3d0b0cfa86eff39f1a1436efa91c734
arturvoloshyn <arthurvoloshyn@gmail.com>
2021-05-24 03:05:46 +0300

    Delete storybook

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,5 @@
-import getMockQuoteTicker from './getMockQuoteTicker';
-import getMockQuoteTickerSymbol from './getMockQuoteTickerSymbol';
 import getSortByField from './getSortByField';
-import getTestStore from './getTestStore';
-import getTestProvider from './getTestProvider';
 import getSortedTickers from './getSortedTickers';
 import getExtClasses from './getExtClasses';
 
-export {
-  getMockQuoteTicker,
-  getMockQuoteTickerSymbol,
-  getSortByField,
-  getTestStore,
-  getTestProvider,
-  getSortedTickers,
-  getExtClasses,
-};
+export { getSortByField, getSortedTickers, getExtClasses };
